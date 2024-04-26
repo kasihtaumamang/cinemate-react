6703a18e2585822b75af037242c941c68c21d420
kasihtaumamang <kasihtaumamang@gmail.com>
2024-04-26 11:36:02 +0700

    update

---
+++ src/pages/MovieList.jsx
@@ -0,0 +1,7 @@
+export const MovieList = () => {
+	return (
+		<main className="text-9xl bg-emerald-200">
+			<div>MovieList</div>;
+		</main>
+	);
+};
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { MovieList } from "./MovieList";
+export { MovieDetail } from "./MovieDetail";
+export { Search } from "./Search";
+export { Page404 } from "./Page404";
